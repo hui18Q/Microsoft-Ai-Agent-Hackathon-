531e783052bb7a393e1b5c472881ac67f339eff1
Mui Rui Xin <ruixinmui78@gmail.com>
2025-04-27 22:31:49 +0800

    resolve

--- frontend/agent_ai_app/lib/main.dart
+++ frontend/agent_ai_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'welcome_page.dart';
+import '../welcome_page.dart';
 import 'login_page.dart';
 import 'signup_page.dart';
 import 'forgot_password_page.dart';
